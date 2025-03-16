5d4481b752f2a85f2aac41fda49734186c043d4c
Darkschmu <164562160+Darkschmu@users.noreply.github.com>
2025-03-16 13:47:04 -0700

    swapped main files

--- qa/main.tf
+++ qa/main.tf
@@ -1,9 +1,9 @@
-module "prod" {
+module "qa" {
   source = "../modules/blog"
 
   environment = {
-    name           = "prod"
-    network_prefix = "10.2"
+    name           = "qa"
+    network_prefix = "10.1"
   }
 
   asg_min_size = 1
