286efa31e5a6a8ad8c82c91f1a9691d88d1d42e0
Darkschmu <164562160+Darkschmu@users.noreply.github.com>
2025-03-15 20:13:55 -0700

    foo

--- main.tf
+++ main.tf
@@ -54,12 +54,15 @@ module "alb" {
   security_groups = [module.blog_sg.security_group_id]
 
   target_groups = {
-    name_prefix      = "blog-"
-    protocol         = "HTTP"
-    port             = 80
-    target_type      = "instance"
-    target_id        = aws_instance.blog.id
-    }
+    ex-instance = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.blog.id
+      }
+  }
+  
 
   tags = {
     Environment = "dev"
